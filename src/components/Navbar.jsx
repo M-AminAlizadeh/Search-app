2776d383a874435e5b9b19e3a6d51173ffa21045
m-aminalizadeh <aminalizadeh@riseup.net>
2024-07-24 12:49:48 +0330

    Build the brand-name part and add a link to it

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import brandIcon from '../assets/search-icon.png'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav>
-      Navbar
+    <nav className="border-b p-5 flex flex-wrap justify-center items-center sm:justify-between">
+      {/* Brand name link */}
+      <Link to="/">
+        <div className="flex border justify-start items-center py-1 px-2 rounded bg-white">
+          <h1 className="font-bold">Brand Name</h1>
+          <img width="40" src={brandIcon} alt="search"/>
+        </div>
+      </Link>
     </nav>
   )
 }
