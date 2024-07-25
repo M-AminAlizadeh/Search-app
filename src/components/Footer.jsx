a9e32b8e7c66a00c4ff040ca0fa22656cf40ea0d
m-aminalizadeh <aminalizadeh@riseup.net>
2024-07-25 09:34:21 +0330

    Complete styling the footer and context

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
 function Footer() {
+  const [currentYear, setCurrentYear] = useState(null)
+  useEffect(() => {
+    const date = new Date()
+    setCurrentYear(date.getFullYear())
+  }, [])
   return (
-    <footer className="border dark:bg-black dark:text-white absolute bottom-0 left-0 right-0">
-      footer      
+    <footer className="border border-slate-300 dark:bg-black dark:text-white dark:border-0 absolute bottom-0 left-0 right-0 text-center py-4">
+      Crafted with ❤️ by 
+      <a href="https://github.com/M-AminAlizadeh" className="mx-1 text-blue-600 hover:border-b-2 hover:border-blue-600" target="_blank">Amin Alizadeh</a>
+      ©️ {currentYear}
     </footer>
   )
 }
