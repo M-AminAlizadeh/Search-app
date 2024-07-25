81eb763cb06cbff63ecb55502e8b7ac7e4074b51
m-aminalizadeh <aminalizadeh@riseup.net>
2024-07-25 07:45:09 +0330

    Add the absolute layout for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="border dark:bg-black dark:text-white">
+    <footer className="border dark:bg-black dark:text-white absolute bottom-0 left-0 right-0">
       footer      
     </footer>
   )
