82262670be2d6864e3cb791b6b31d9273717ec34
m-aminalizadeh <aminalizadeh@riseup.net>
2024-07-25 08:03:16 +0330

    Add style to search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,9 +5,11 @@ function Search({setQuery}) {
     setQuery(e.target.value)
   }
   return (
-    <div>
-      <input type="text" name="" id="" onChange={(e) => handleSearch(e)}/>
-      <Link to="/search">search btn</Link>
+    <div className="relative rounded-full">
+      <input className="py-2 px-4 rounded-full border border-blue-400 dark:bg-black dark:text-white" type="text" onChange={(e) => handleSearch(e)} placeholder="Enter something"/>
+      <Link to="/search" className="border border-blue-400 flex justify-center items-center bg-white dark:bg-black p-2 absolute top-0 bottom-0 right-0 rounded-r-full">
+        <img width="28" src="https://img.icons8.com/emoji/48/magnifying-glass-tilted-right-emoji.png" alt="magnifying-glass-tilted-right-emoji" />
+      </Link>
     </div>
   )
 }
